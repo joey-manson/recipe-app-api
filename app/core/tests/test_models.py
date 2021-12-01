567c9da9b81ce105b92c798a49523a6880894073
joey <mendesesousa@gmail.com>
2021-12-01 16:22:23 +0000

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,54 +11,65 @@ def sample_user(email='test@gmail.com', password='testpass'):
 
 class ModelTests(TestCase):
 
-        def test_create_user_with_email_successful(self):
-            """Test creating a new user with an email is successful"""
-            email = 'test@gmail.com'
-            password = 'Testpass123'
-            user = get_user_model().objects.create_user(
-                email=email,
-                password=password
-            )
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
-            self.assertEqual(user.email, email)
-            self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
-        def test_new_user_email_normalized(self):
-            """Test the email for a new user is normalized"""
-            email = 'test@GMAIL.COM'
-            user = get_user_model().objects.create_user(email, 'test123')
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
 
-            self.assertEqual(user.email, email.lower())
+        self.assertEqual(user.email, email.lower())
 
-        def test_new_user_invalid_email(self):
-            """Test creating user with no email raises error"""
-            with self.assertRaises(ValueError):
-                get_user_model().objects.create_user(None, 'test123')
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
 
-        def test_create_new_superuser(self):
-            """Test creating a new superuser"""
-            user = get_user_model().objects.create_superuser(
-                'test@gmail.com',
-                'test123'
-            )
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
 
-            self.assertTrue(user.is_superuser)
-            self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
-        def test_tag_str(self):
-            """Test the tag string representation"""
-            tag = models.Tag.objects.create(
-                user=sample_user(),
-                name='Vegan'
-            )
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
 
-            self.assertEqual(str(tag), tag.name)
+        self.assertEqual(str(tag), tag.name)
 
-        def test_ingredient_str(self):
-            """Test the ingredient string representation"""
-            ingredient = models.Ingredient.objects.create(
-                user=sample_user(),
-                name='Cucumber'
-            )
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
 
-            self.assertEqual(str(ingredient), ingredient.name)
+        self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Stake and muchroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
